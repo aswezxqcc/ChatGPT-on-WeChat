bd966a542fa80fd4059c086968d26c699a2b3b43
qichenchu <aswezxqcc@live.cn>
2023-03-28 23:33:35 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -15,9 +15,9 @@ if (fs.existsSync("./config.yaml")) {
 // if 'config.yaml' not exist, read them from env
 else {
   configFile = {
-    openaiApiKey: process.env.OPENAI_API_KEY,
-    openaiOrganizationID: process.env.OPENAI_ORGANIZATION_KEY,
-    chatgptTriggerKeyword: process.env.CHATGPT_TRIGGER_KEYWORD,
+    openaiApiKey: "1",
+    openaiOrganizationID: "org-TsxYBQWs4JydJaXjfi6aUKdX",
+    chatgptTriggerKeyword: "ll",
   };
 }
 
